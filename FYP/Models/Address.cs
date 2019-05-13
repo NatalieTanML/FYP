d9305291c11e93af77ba43cba4a59f5a015deacc
nat <natalietan613@gmail.com>
2019-05-13 11:26:24 +0800

    Add project files.

---
+++ FYP/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string PostalCode { get; set; }
+        public string UnitNo { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string HotelName { get; set; }
+    }
+}
---
+++ FYP/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CurrentQuantity { get; set; }
+        public int MinimumQuantity { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public User User { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
