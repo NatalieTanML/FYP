2631b0d2b3c84428e5aba64275b70ad36f1343c9
Neo Tian How <tianhow1234@hotmail.com>
2019-05-25 14:06:35 +0800

    Add project files.

---
+++ FYP/APIs/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FYP.Helpers;
+using FYP.Models;
+using FYP.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FYP.APIs
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private IProductService _productService;
+        private readonly AppSettings _appSettings;
+
+        public ProductsController(IProductService productService, IOptions<AppSettings> appSettings)
+        {
+            _productService = productService;
+            _appSettings = appSettings.Value;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetOrder();
+            List<object> productList = new List<object>();
+            foreach (Product product in products)
+            {
+                productList.Add(new
+                {
+                    productId = product.ProductId,
+                    productName = product.ProductName,
+                    price = product.Price,
+                    categoryName = product.Category.CategoryName,
+                    description = product.Description,
+                    currentQuantity = product.CurrentQuantity,
+                    minimumQuantity = product.MinimumQuantity,
+                    updatedBy = product.UpdatedBy
+                });
+            }
+            return new JsonResult(productList);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("getproductbyid/{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            try
+            {
+                var product = await _productService.GetById(id);
+                return Ok(new
+                {
+                    productId = product.ProductId,
+                    productName = product.ProductName,
+                    price = product.Price,
+                    categoryName = product.Category.CategoryName,
+                    description = product.Description,
+                    currentQuantity = product.CurrentQuantity,
+                    minimumQuantity = product.MinimumQuantity,
+                    updatedBy = product.UpdatedBy
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost("getOrder")]
+        public async Task<IActionResult> GetPayPalOrder([FromForm] IFormCollection inFormData)
+        {
+            var orderId = inFormData["orderId"];
+            var order = await _productService.GetPayPalOrder(orderId);
+            return new JsonResult(order);
+        }
+    }
+}
---
+++ FYP/Services/ProductServices.cs
@@ -0,0 +1,65 @@
+﻿using BraintreeHttp;
+using FYP.Data;
+using FYP.Helpers;
+using FYP.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PayPalCheckoutSdk.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetOrder();
+        Task<Product> GetById(int id);
+        Task<HttpResponse> GetPayPalOrder (String orderId);
+    }
+
+    public class ProductService : IProductService
+    {
+        private ApplicationDbContext _context;
+        private readonly AppSettings _appSettings;
+
+        public ProductService(ApplicationDbContext context, IOptions<AppSettings> appSettings)
+        {
+            _context = context;
+            _appSettings = appSettings.Value;
+        }
+
+        public async Task<IEnumerable<Product>> GetOrder()
+        {
+            return await _context.Products.Include(product => product.Category).ToListAsync();
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            return await _context.Products.Include(product => product.Category).FirstOrDefaultAsync(p => p.ProductId == id);
+        }
+
+        public async Task<HttpResponse> GetPayPalOrder(string orderId)
+        {
+            OrdersGetRequest request = new OrdersGetRequest(orderId);
+            //3. Call PayPal to get the transaction
+            var response = await PayPalClient.client().Execute(request);
+            //4. Save the transaction in your database. Implement logic to save transaction to your database for future reference.
+            var result = response.Result<Order>();
+            Console.WriteLine("Retrieved Order Status");
+            Console.WriteLine("Status: {0}", result.Status);
+            Console.WriteLine("Order Id: {0}", result.Id);
+            Console.WriteLine("Intent: {0}", result.Intent);
+            Console.WriteLine("Links:");
+            foreach (LinkDescription link in result.Links)
+            {
+                Console.WriteLine("\t{0}: {1}\tCall Type: {2}", link.Rel, link.Href, link.Method);
+            }
+            AmountWithBreakdown amount = result.PurchaseUnits[0].Amount;
+            Console.WriteLine("Total Amount: {0} {1}", amount.CurrencyCode, amount.Value);
+
+            return response;
+        }
+    }
+}
